6c924a85d3f1932a6b6352a28f94b8589c171476
f-yamaguchi@cresco <f-yamaguchi@cresco.co.jp>
2019-10-18 20:15:34 +0900

    更新

--- MoveFilefForAmazoness/MoveFilefForAmazoness/MoveFIleForAmazoness.cs
+++ MoveFilefForAmazoness/MoveFilefForAmazoness/MoveFIleForAmazoness.cs
@@ -12,6 +12,14 @@ namespace MoveFilefForAmazoness
 {
     public partial class MoveFIleForAmazoness : Form
     {
+        static string _FromFilePath;
+        static string _ToFilePath;
+        static string _FromFileName;
+        static string _ToFileName;
+        static DateTime _FromFileTime;
+        static DateTime _ToFileTime;
+
+
         public MoveFIleForAmazoness()
         {
             InitializeComponent();
@@ -26,7 +34,7 @@ namespace MoveFilefForAmazoness
         {
             DialogResult dr = openFileDialog1.ShowDialog();
             if (dr == System.Windows.Forms.DialogResult.OK){
-                textBox1.Text = openFileDialog1.FileName;
+                FromFilePathTextBox.Text = openFileDialog1.FileName;
             }
         }
 
@@ -34,7 +42,171 @@ namespace MoveFilefForAmazoness
         {
             DialogResult dr = openFileDialog1.ShowDialog();
             if (dr == System.Windows.Forms.DialogResult.OK){
-                textBox2.Text = openFileDialog1.FileName;
+                ToFilePathTextBox.Text = openFileDialog1.FileName;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MoveFIleForAmazoness_Load(object sender, EventArgs e)
+        {       
+            string From = System.Configuration.ConfigurationSettings.AppSettings["FromFilePath"];
+            string To = System.Configuration.ConfigurationSettings.AppSettings["ToFilePath"];
+
+            if (From != "")
+            {
+                _FromFilePath = From;
+                FromFilePathTextBox.Text = _FromFilePath;
+            }
+
+            if(To != "")
+            {
+                _ToFilePath = To;
+                ToFilePathTextBox.Text = _ToFilePath;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            
+            if (FromFilePathTextBox.Text == "" | ToFilePathTextBox.Text == "")
+            {
+                MessageBox.Show("パスが設定されていません。",
+                                "エラー",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+
+                return;
+            }
+
+            
+            //フルパス
+            _FromFilePath = FromFilePathTextBox.Text;
+            _ToFilePath = ToFilePathTextBox.Text;
+
+            //ファイル名
+            _FromFileName = System.IO.Path.GetFileName(_FromFilePath);
+            _ToFileName = System.IO.Path.GetFileName(_ToFilePath);
+
+            //ファイルの更新日付
+            _FromFileTime = System.IO.File.GetLastWriteTime(_FromFilePath);
+            _ToFileTime = System.IO.File.GetLastWriteTime(_ToFilePath);
+
+            FromFileName.Visible = true;
+            FromFileName.Text = _FromFileName;
+                        
+            ToFileName.Visible = true;
+            ToFileName.Text = _ToFileName;
+            
+            FromFileTime.Visible = true;
+            FromFileTime.Text = _FromFileTime.ToString() ;
+
+            ToFileTime.Visible = true;
+            ToFileTime.Text = _ToFileTime.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (FromFilePathTextBox.Text == "" | ToFilePathTextBox.Text == "")
+            {
+                MessageBox.Show("パスが設定されていません。",
+                                "エラー",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+
+                return;
+            }
+
+            if (_FromFileName != _ToFileName)
+            {
+                MessageBox.Show("ファイルが一致していません。",
+                                "エラー",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+
+                return;
+
+            }
+            else
+            {
+                if (_ToFileTime < _FromFileTime)
+                {
+                    DialogResult result = MessageBox.Show("移動先の方が更新日が新しくなっています移動しますか？",
+                                    "ファイル移動しますか？",
+                                    MessageBoxButtons.YesNo,
+                                    MessageBoxIcon.Exclamation,
+                                    MessageBoxDefaultButton.Button2);
+
+                    switch (result)
+                    {
+                        case DialogResult.OK:
+
+                            if(BackupOK.Checked == true)
+                            {
+                                System.IO.File.Move(_ToFilePath, _ToFilePath + DateTime.Now.ToString("yyyyMMddhhmmss"));
+                                System.IO.File.Copy(_FromFilePath, _ToFilePath);
+                                MessageBox.Show("ファイルのコピーが完了しました。",
+                                                "コピー完了",
+                                                MessageBoxButtons.OK,
+                                                MessageBoxIcon.Information);
+                            
+                            }
+                            else
+                            {
+                                System.IO.File.Copy(_FromFilePath, _ToFilePath, true);
+                                MessageBox.Show("ファイルのコピーが完了しました。",
+                                                "コピー完了",
+                                                MessageBoxButtons.OK,
+                                                MessageBoxIcon.Information);
+                            }
+
+                            break;
+
+                        case DialogResult.No:
+                            break;
+
+                        case DialogResult.Cancel:
+                            break;
+
+                        default:
+                            break;
+
+                    }
+
+                }
+                else
+                {
+                    if (BackupOK.Checked == true)
+                    {
+                        System.IO.File.Move(_ToFilePath, _ToFilePath + DateTime.Now.ToString("yyyyMMddhhmmss"));
+                        System.IO.File.Copy(_FromFilePath, _ToFilePath);
+                        MessageBox.Show("ファイルのコピーが完了しました。",
+                                        "コピー完了",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Information);
+
+                    }
+                    else
+                    {
+                        System.IO.File.Copy(_FromFilePath, _ToFilePath, true);
+                        MessageBox.Show("ファイルのコピーが完了しました。",
+                                        "コピー完了",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Information);
+                    }
+
+                }
+
             }
 
         }
