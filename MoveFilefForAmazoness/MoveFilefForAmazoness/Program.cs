47a89d51a77aca2338af2dc65accbe3e754cc41f
zaqbigface <zaqbigface@gmail.com>
2019-10-21 21:40:20 +0900

    更新

--- MoveFilefForAmazoness/MoveFilefForAmazoness/Program.cs
+++ MoveFilefForAmazoness/MoveFilefForAmazoness/Program.cs
@@ -16,7 +16,7 @@ namespace MoveFilefForAmazoness
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MoveFIleForAmazoness());
+            Application.Run(new Amazoness());
         }
     }
 }
